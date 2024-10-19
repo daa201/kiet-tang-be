7b15b3e39dd3b788a9dc693673244a90dbb811f1
daa201 <139944607+daa201@users.noreply.github.com>
2024-10-19 09:35:27 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = ' em bé ';
+const giftUrl = 'https://www.facebook.com/PandaCute.Official/';
+const eventName = 'Chúc 20-10 tới';
+const titleCard = 'Tặng bé iu của anh';
+const contentCard = 'Chúc honey của anh 20/10 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
+
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
